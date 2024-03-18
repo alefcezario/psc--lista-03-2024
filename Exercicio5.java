6e221e16de18da62015192bdd22929b811bb6b2a
Alef Cezario <161400415+alefcezario@users.noreply.github.com>
2024-03-17 21:48:02 -0300

    Create Exercicio5.java
    5. Escrever um algoritmo que leia a partir do teclado dois números reais e um dos
    símbolos de operação: + , - , * , / ou ^ .
    O algoritmo deve retornar o resultado da operação.
    Caso o símbolo informado seja diferente dos símbolos preestabelecidos, o algoritmo
    deve apresentar uma mensagem de erro, informando que o símbolo da operação é
    inválido.

---
+++ Exercicio5.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Exercicio5 {
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double primeiroNumero, segundoNumero, resultadoOperacao;
+        char simboloOperacao;
+
+        System.out.println("Digite o primeiro numero real:");
+        primeiroNumero = scanner.nextDouble();
+
+        System.out.println("Digite o segundo numero real:");
+        segundoNumero = scanner.nextDouble();
+
+        System.out.println("Digite o símbolo para a operação (+, -, *, /, ^):");
+        simboloOperacao = scanner.next().charAt(0);
+      
+        resultadoOperacao = 0;
+        switch (simboloOperacao) {
+            case '-':
+            resultadoOperacao = primeiroNumero - segundoNumero;
+                break;
+
+            case '+':
+            resultadoOperacao = primeiroNumero + segundoNumero;
+                break;
+
+            case '*':
+            resultadoOperacao = primeiroNumero * segundoNumero;
+                break;
+                
+            case '/':
+                if (segundoNumero != 0) {
+                    resultadoOperacao = primeiroNumero / segundoNumero;
+                } else {
+                    System.out.println("Não é possivel dividir por zero!");
+                    return;
+                }
+                break;
+
+            case '^':
+            resultadoOperacao = Math.pow(primeiroNumero, segundoNumero);
+                break;
+            default:
+                System.out.println("Simbolo diferente dos solicitados. Favor inserir apenas os simbolos (+, -, *, /, ^)");
+                return;
+        }
+
+        System.out.println("O resultado é: " + resultadoOperacao);
+
+        scanner.close();
+    }
+}
