69dea26db67f4ffe3907cf195bcdf4a1b3193df7
Alef Cezario <161400415+alefcezario@users.noreply.github.com>
2024-03-17 22:11:57 -0300

    Create Exercicio2.java
    2. Escreva o algoritmo de uma máquina de venda automática de salgadinhos, doces,
    sucos e refrigerantes. O algoritmo deve calcular o menor número de notas que deve
    ser dado de troco para um pagamento efetuado.
    O algoritmo deve ler o valor da compra e o valor pago. Se o valor pago for menor que
    o valor da compra, a máquina deve apresentar uma mensagem, informando que a
    quantia paga é insuficiente para realizar a compra. A máquina aceita apenas notas de
    R$ 50,00, R$ 20,00, R$ 10,00, R$ 5,00, R$ 2,00 e R$ 1,00.
    Exemplo:
    Valor pago: R$ 100,00
    Valor da compra: R$ 23,00
    Troco: R$ 77,00
    Notas de R$ 50,00: 1
    Notas de R$ 20,00: 1
    Notas de R$ 10,00: 0
    Notas de R$ 5,00: 1
    Notas de R$ 2,00: 1
    Notas de R$ 1,00: 0

---
+++ Exercicio2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double valorDaCompra, valorPago,troco;
+        
+        System.out.println("Digite o valor da sua compra R$");
+        valorDaCompra = scanner.nextDouble();
+
+        System.out.println("Agora digite o valor que vc ia pagar");
+        valorPago = scanner.nextDouble();
+        
+  
+        if (valorPago < valorDaCompra) {
+            System.out.println("O valor que vc pagou é menos do que o que vc precisa pagar.");
+            return;
+        }
+
+        troco = valorPago - valorDaCompra;
+        System.out.println("Troco R$" + troco);
+
+        int[] notas={50, 20, 10, 5, 2, 1 };
+        for (int nota : notas) {
+            int quantidadeNotas=(int) (troco / nota);
+            if (quantidadeNotas>0) {
+                System.out.println("Notas de R$"+nota+": "+quantidadeNotas);
+                troco%=nota;
+            }
+        }
+
+        scanner.close();
+    }    
+}
